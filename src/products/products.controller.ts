6a81d392bae224a83b95cf81f9cd8a8aaab35420
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-12-19 12:59:49 -0500

    🌟ADD: Update Product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   Query,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -33,8 +34,11 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -67,8 +67,19 @@ export class ProductsService {
     return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product ${updateProductDto}`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({
+      id,
+      ...updateProductDto,
+    });
+    if (!product) throw new NotFoundException(`Product with ${id} not found`);
+
+    try {
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handlerDBException(error, 'Unexpected error updating product');
+    }
   }
 
   async remove(id: string) {
