645e5039ed21a34298a6594435683814768c0b95
Luis Miguel Del Gallego Horta <luis.delgallego@licify.co>
2024-01-09 11:05:51 -0500

    🌟ADD(UpLoadFile): Save file into directory

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -8,13 +8,22 @@ import {
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file', { fileFilter: fileFilter }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      // limits: { fileSize: 1000 },
+      storage: diskStorage({
+        destination: './static/uploads',
+      }),
+    }),
+  )
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     if (!file)
       throw new BadRequestException('Make sure you are sending a image');
