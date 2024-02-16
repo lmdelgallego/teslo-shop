ada6f69ae9ff6c99f4dbb2c46b4cc95a22613979
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2024-02-15 21:45:00 -0500

    🌟ADD(Auth): Verify User Role

--- src/auth/guards/user-role.guard.ts
+++ src/auth/guards/user-role.guard.ts
@@ -1,6 +1,13 @@
 import { Reflector } from '@nestjs/core';
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -13,7 +20,23 @@ export class UserRoleGuard implements CanActivate {
       'roles',
       context.getHandler(),
     );
-    console.log(validRoles);
+
+    if (!validRoles) return true;
+    if (validRoles.length === 0) return true;
+
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
+
+    if (!user) throw new BadRequestException('User not found (Guard)');
+
+    for (const role of user.roles) {
+      if (validRoles.includes(role)) return true;
+    }
+
+    throw new ForbiddenException(
+      'You do not have permission to access this route',
+    );
+
     return true;
   }
 }
