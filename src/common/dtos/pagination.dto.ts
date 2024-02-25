d07121180e24723d9526e85c9e1ded3f039c9eef
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2024-02-25 16:55:30 -0500

    🌟ADD(Documentation): Added DTOs Documentation - NestJs-Swagger

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,12 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive, Min } from 'class-validator';
 
 export class PaginationDTO {
+  @ApiProperty({
+    type: Number,
+    description: 'Limit the number of records',
+    default: 10,
+  })
   @IsOptional()
   @IsPositive()
   @Type(() => Number)
   limit?: number;
 
+  @ApiProperty({
+    type: Number,
+    description: 'Offset the records',
+    default: 0,
+  })
   @IsOptional()
   @Min(0)
   @Type(() => Number)
